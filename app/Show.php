f528cd9adc54a97ecf79c564aa97594e451cd6c1
kd9205 <kd9205@mail.rit.edu>
2017-10-15 23:16:07 -0400

    creating show type

---
+++ app/Show.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Show extends Model
+{
+    protected $fillable = ['name', 'description', 'entry_cap', 'submission_start', 'submission_end', 'judging_start', 'judging_end']
+}
---
+++ database/migrations/2017_10_16_030004_create_shows_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShowsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shows', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->integer('entry_cap');
+            $table->dateTime('submission_start');
+            $table->dateTime('submission_end');
+            $table->dateTime('judging_start');
+            $table->dateTime('judging_end');
+            $table->timestamps();            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shows');
+    }
+}
