9073ba54f2d4aadfdbb00782b81de938f06d67a8
Bill Dybas <wmdybas@gmail.com>
2017-11-05 13:40:22 -0500

    Add Initial Manage Judges Page
    - Sortable Table w/ Multiselect Checkboxes

--- frontend/src/Admin/Page.js
+++ frontend/src/Admin/Page.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import ViewShow from './pages/ViewShow'
 import CreateShow from './pages/CreateShow'
+import ManageJudges from './pages/ManageJudges'
 
 import Layout from './components/Layout'
 import NotFound from '../shared/components/NotFound'
@@ -14,6 +15,7 @@ const Admin = () => (
       <Route exact path='/admin' component={Dashboard} />
       <Route exact path='/admin/show/create' component={CreateShow} />
       <Route exact path='/admin/show/:id' component={ViewShow} />
+      <Route exact path='/admin/show/:id/judges' component={ManageJudges} />
       <Route component={NotFound} />
     </Switch>
   </Layout>
