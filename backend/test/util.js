ee96463f770d7644d13cd59bdc1901dc3f42013c
Robert McLaughlin <robert@sparkk.us>
2017-11-06 21:31:13 -0500

    add login tests

--- backend/test/util.js
+++ backend/test/util.js
@@ -17,3 +17,22 @@ function sign (payload) {
 export function signUserToken(user) {
   return sign(user.dataValues);
 }
+
+/**
+ * Decodes a token and returns it
+ * @param {string} token the jwt token
+ * @return {object} decoded token
+ */
+export function decodeUserToken(token) {
+  var decoded = {};
+  jwt.verify(
+    token,
+    nconf.get('auth:jwt:pub'),
+    { algorithm: 'RS256' },
+    (err, decoded_) => {
+      if (!err) {
+        decoded = decoded_;
+      }
+  });
+  return decoded
+}
