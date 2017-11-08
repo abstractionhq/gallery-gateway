e8c5eab97074aafd9f5cf30ab1f09f3693bb0a11
Bill Dybas <wmdybas@gmail.com>
2017-11-08 15:56:57 -0500

    Fix Capitalization

--- backend/resolvers/index.js
+++ backend/resolvers/index.js
@@ -1,7 +1,7 @@
 import User from './types/userType'
 import DateScalar from './types/dateScalar'
 import CreateShow from './mutations/createShow'
-import * as ShowQuery from './queries/ShowQuery'
+import * as ShowQuery from './queries/showQuery'
 import * as UserQuery from './queries/userQuery'
 import * as JudgeMutations from './mutations/judge'
 
