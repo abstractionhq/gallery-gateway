38b8ae0d814a45fb76d571723b63dd43f8fea606
Bill Dybas <wmdybas@gmail.com>
2017-11-08 08:00:48 -0500

    Add Create Show Markup

--- frontend/src/Admin/pages/CreateShow.js
+++ frontend/src/Admin/pages/CreateShow.js
@@ -1,15 +1,55 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Container, Row, Col } from 'reactstrap'
+import React, { Component } from 'react'
+import { Container, Row, Col, Form, FormGroup, Label, Input, Button } from 'reactstrap'
 
-const CreateShow = () => (
-  <Container>
-    <Row>
-      <Col>
-        <h1>CreateShow</h1>
-      </Col>
-    </Row>
-  </Container>
-)
+export default class CreateShow extends Component {
+  render () {
+    return (
+      <Container>
+        <h1>Create Show</h1>
+        <Row>
+          <Col>
+            <Form>
+              {/* TODO: There should be client-side validation on these form fields */}
+              <FormGroup>
+                <Label for='name' >Name</Label>
+                <Input id='name' name='name' />
+              </FormGroup>
+              <FormGroup>
+                <Label for='description'>Description</Label>
+                <Input type='textarea' id='description' name='description' rows={3} />
+              </FormGroup>
+              <FormGroup>
+                {/* TODO: Should validate positive */}
+                <Label for='submission-limit'>Submission Limit</Label>
+                <Input type='number' id='submission-limit' name='entry_cap' />
+              </FormGroup>
+              {/* TODO: Side-by-side fields for start/end ? */}
+              <FormGroup>
+                {/* TODO: Should validate the dates are after one another */}
+                <Label for='submission-start'>Submission Starts</Label>
+                <Input type='date' id='submission-start' name='submission_start' />
+              </FormGroup>
+              <FormGroup>
+                <Label for='submission-end'>Submission Ends</Label>
+                <Input type='date' id='submission-end' name='submission_end' />
+              </FormGroup>
+              <FormGroup>
+                <Label for='judging-start'>Judging Starts</Label>
+                <Input type='date' id='judging-start' name='judging_start' />
+              </FormGroup>
+              <FormGroup>
+                <Label for='judging-end'>Judging Ends</Label>
+                <Input type='date' id='judging-end' name='judging_end' />
+              </FormGroup>
 
-export default CreateShow
+              {/* TODO: This currently reloads the page w/ the form contents as query-strings. This
+                should instead intercept the submit action and send the data through a GraphQL mutation.
+               */}
+              <Button type='submit' color='primary'>Submit</Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
