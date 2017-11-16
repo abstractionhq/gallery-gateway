8533abe50d1adfceebea87ee10c6aaaeccde8f13
Robert McLaughlin <robert@sparkk.us>
2017-11-15 23:04:03 -0500

    add all env variables to travis

--- backend/test/routes/auth.js
+++ backend/test/routes/auth.js
@@ -42,10 +42,6 @@ describe('Authentication', () => {
       request(server)
         .post('/auth/login/callback')
         .expect((res) => {
-          console.log(res.headers)
-          console.log(res.body)
-          console.log(res.statusCode)
-          console.log(res.text)
           expect(res.headers).to.have.property('location')
           expect(
             res.headers.location.indexOf(nconf.get('auth:saml:entryPoint'))
