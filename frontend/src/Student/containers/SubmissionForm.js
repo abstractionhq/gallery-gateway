a087e7f7a4b0717af20c7fef347f95525ed4d9ff
Bill Dybas <wmdybas@gmail.com>
2017-12-08 01:00:05 -0500

    Add Photo Entry Mutations

--- frontend/src/Student/containers/SubmissionForm.js
+++ frontend/src/Student/containers/SubmissionForm.js
@@ -1,19 +1,32 @@
-import { graphql } from 'react-apollo'
+import { graphql, compose } from 'react-apollo'
 import { push } from 'connected-react-router'
 import { connect } from 'react-redux'
 
 import { uploadImage } from '../actions'
 
 import SubmissionForm from '../components/SubmissionForm'
+import CreatePhotoEntry from '../mutations/createPhotoEntry.graphql'
 
 const mapStateToProps = (state) => ({
   previewImage: state.student.ui.submission.previewImage || {}
 })
 
 const mapDispatchToProps = (dispatch) => ({
+  done: () => dispatch(push('/student/')),
   handleUpload: (file) => dispatch(uploadImage(file))
 })
 
 const withRedux = connect(mapStateToProps, mapDispatchToProps)(SubmissionForm)
 
-export default withRedux
+const withMutations = compose(
+  graphql(CreatePhotoEntry, {
+    props: ({mutate}) => ({
+      submit: (entry) => mutate({
+        variables: { input: entry }
+      })
+    })
+  })
+  // TODO: Video and Physical Object Mutations
+)(withRedux)
+
+export default withMutations
