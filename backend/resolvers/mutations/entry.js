3384e201a7081196b39bf02765029aefb8175023
Kayla Davis <kd9205@rit.edu>
2017-12-12 16:44:58 -0500

    pr changes

--- backend/resolvers/mutations/entry.js
+++ backend/resolvers/mutations/entry.js
@@ -3,6 +3,7 @@ import Entry from '../../models/entry'
 import Image from '../../models/image'
 import Video from '../../models/video'
 import { ADMIN, IMAGE_ENTRY } from '../../constants'
+import {allowedToSubmit, parseVideo} from '../../helpers/submittion'
 
 export function createPhoto (_, args, req) {
   if (req.auth.type !== ADMIN && !allowedToSubmit(args, req)) {
@@ -42,20 +43,4 @@ export function createVideo (_, args, req) {
       entryId: video.id
     }).then((entry) => Object.assign(entry, video.dataValues))
   })
-}
-
-function allowedToSubmit (args, req) {
-  // TODO update the below to account for group submission rights
-  return req.auth.username !== undefined && req.auth.username === args.input.entry.studentUsername
-}
-
-function parseVideo (url) {
-  url.match(/(http:|https:|)\/\/(player.|www.)?(vimeo\.com|youtu(be\.com|\.be|be\.googleapis\.com))\/(video\/|embed\/|watch\?v=|v\/)?([A-Za-z0-9._%-]*)(&\S+)?/)
-  let type
-  if (RegExp.$3.indexOf('youtu') > -1) {
-    type = 'youtube'
-  } else if (RegExp.$3.indexOf('vimeo') > -1) {
-    type = 'vimeo'
-  }
-  return { type: type, id: RegExp.$6 }
 }
