044cdd3fc668606ae85eb2fe3a29f7836d704d97
Bill Dybas <wmdybas@gmail.com>
2018-01-21 13:14:39 -0500

    Fix Typo in File Name
    submittion -> submission
    
    Also add failing YouTube URL test case

--- backend/test/helpers/submission.js
+++ backend/test/helpers/submission.js
@@ -1,8 +1,9 @@
-import { parseVideo } from '../../helpers/submittion'
 import { expect } from 'chai'
 
+import { parseVideo } from '../../helpers/submission'
+
 describe('Parse Video URL', function () {
-  it('accepts valid youtube urls', function () {
+  it('Accepts valid YouTube URLs', function (done) {
     expect(parseVideo('https://www.youtube.com/watch?v=12345678901').id).to.equal('12345678901')
     expect(parseVideo('https://www.youtube.com/watch?v=12345678902').id).to.equal('12345678902')
     expect(parseVideo('http://www.youtube.com/watch?v=12345678903').id).to.equal('12345678903')
@@ -10,9 +11,11 @@ describe('Parse Video URL', function () {
     expect(parseVideo('http://youtube.com/watch?v=12345678905').id).to.equal('12345678905')
     expect(parseVideo('http://youtu.be/12345678906').id).to.equal('12345678906')
     expect(parseVideo('http://youtu.be/12345678906').type).to.equal('youtube')
-    return Promise.resolve()
+    // TODO: Failing Test Case
+    expect(parseVideo('https://www.youtube.com/watch?t=1s&v=12345678901').id).to.equal('12345678901')
+    done()
   })
-  it('accepts valid vimeo urls', function () {
+  it('Accepts valid Vimeo URLs', function (done) {
     expect(parseVideo('http://vimeo.com/1234').id).to.equal('1234')
     expect(parseVideo('https://vimeo.com/1234').id).to.equal('1234')
     expect(parseVideo('https://www.vimeo.com/1234').id).to.equal('1234')
@@ -21,6 +24,6 @@ describe('Parse Video URL', function () {
     expect(parseVideo('https://vimeo.com/channels/staffpicks/40004003').id).to.equal('40004003')
     expect(parseVideo('https://vimeo.com/album/3953264/video/166790295').id).to.equal('166790295')
     expect(parseVideo('https://vimeo.com/groups/motion/videos/73234724').id).to.equal('73234724')
-    return Promise.resolve()
+    done()
   })
 })
