4f483d20fcb527213c56033c6c140b5fed0ba043
kayladavis <kd9205@rit.edu>
2018-01-23 18:45:53 -0500

    Config changes to remove sequelize operators deprecation warning and adding npm config to run single test (#126)
    * config changes to remove deprecation warning
    * adding ability to test single file
    * updating readme

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,48 @@
 import config from './index'
+import Sequelize from 'sequelize'
 
 const DB = config.get('ABSTRACTION_DB_NAME')
 const USER = config.get('ABSTRACTION_DB_USER')
 const PASSWORD = config.get('ABSTRACTION_DB_PASS')
 const HOST = config.get('ABSTRACTION_DB_HOST')
+
+const Op = Sequelize.Op;
+const operatorAliases =  {
+  $eq: Op.eq,
+  $ne: Op.ne,
+  $gte: Op.gte,
+  $gt: Op.gt,
+  $lte: Op.lte,
+  $lt: Op.lt,
+  $not: Op.not,
+  $in: Op.in,
+  $notIn: Op.notIn,
+  $is: Op.is,
+  $like: Op.like,
+  $notLike: Op.notLike,
+  $iLike: Op.iLike,
+  $notILike: Op.notILike,
+  $regexp: Op.regexp,
+  $notRegexp: Op.notRegexp,
+  $iRegexp: Op.iRegexp,
+  $notIRegexp: Op.notIRegexp,
+  $between: Op.between,
+  $notBetween: Op.notBetween,
+  $overlap: Op.overlap,
+  $contains: Op.contains,
+  $contained: Op.contained,
+  $adjacent: Op.adjacent,
+  $strictLeft: Op.strictLeft,
+  $strictRight: Op.strictRight,
+  $noExtendRight: Op.noExtendRight,
+  $noExtendLeft: Op.noExtendLeft,
+  $and: Op.and,
+  $or: Op.or,
+  $any: Op.any,
+  $all: Op.all,
+  $values: Op.values,
+  $col: Op.col
+}
 
 const development = {
   dialect: 'mysql',
@@ -11,7 +50,8 @@ const development = {
   database: DB,
   username: USER,
   password: PASSWORD,
-  port: 3306
+  port: 3306,
+  operatorAliases: operatorAliases
 }
 
 const test = {
@@ -21,7 +61,8 @@ const test = {
   username: USER,
   password: PASSWORD,
   port: 3306,
-  logging: false
+  logging: false,
+  operatorsAliases: operatorAliases
 }
 
 // Additonal exports so that sequelize-cli can read this config properly
