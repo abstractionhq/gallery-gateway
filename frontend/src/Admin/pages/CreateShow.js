abfa0dae10a4c9caf1a6ecc484a7d71a0edf26ee
Bill <wmdybas@gmail.com>
2018-01-24 18:21:01 -0500

    Comment Out Code
    Currently Throwing a Type Error Preventing the App from Running
    
    Will Investigate Further Later

--- frontend/src/Admin/pages/CreateShow.js
+++ frontend/src/Admin/pages/CreateShow.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Container } from 'reactstrap'
 
-import CreateShowForm from '../containers/CreateShowForm'
+// import CreateShowForm from '../containers/CreateShowForm'
 
 const CreateShow = () => (
   <Container>
     <h1>Create Show</h1>
-    <CreateShowForm />
+    {/*<CreateShowForm />*/}
   </Container>
 )
 
