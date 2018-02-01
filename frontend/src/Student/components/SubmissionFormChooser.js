73e8ad4130cdabda7412ccbe3739879c53489dbd
Robert McLaughlin <robert@sparkk.us>
2018-02-01 15:57:11 -0500

    stylize form submission type chooser

--- frontend/src/Student/components/SubmissionFormChooser.js
+++ frontend/src/Student/components/SubmissionFormChooser.js
@@ -2,6 +2,9 @@ import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col } from 'reactstrap'
 import styled from 'styled-components'
+import FaImage from 'babel-loader!react-icons/fa/image'
+import FaVideo from 'babel-loader!react-icons/fa/video-camera'
+import FaLightbulb from 'babel-loader!react-icons/fa/lightbulb-o'
 
 const Header = styled.h2`
   margin-top: 25px;
@@ -18,11 +21,9 @@ const Header = styled.h2`
 `
 
 const SubmissionContainer = styled.section`
-  border: 2px solid black;
-  border-radius: 10px;
   margin-bottom: 25px;
   min-height: 250px;
-  padding-top: 100px;
+  padding-top: 70px;
   text-align: center;
 `
 
@@ -31,23 +32,32 @@ const SubmissionFormChooser = (props) => (
     <Header>What are you submitting?</Header>
     <Row>
       <Col xs='12' sm='6' md='4'>
-        <Link to='/submit/photo' style={{textDecoration: 'none'}}>
-          <SubmissionContainer className='bg-light text-dark'>
+        <Link to='/submit/photo'
+          style={{textDecoration: 'none', display: 'block'}}
+          className="bg-light">
+          <SubmissionContainer className='text-dark'>
+            <FaImage size="4em" />
             <h3>Photo</h3>
           </SubmissionContainer>
         </Link>
       </Col>
       <Col xs='12' sm='6' md='4'>
-        <Link to='/submit/video' style={{textDecoration: 'none'}}>
-          <SubmissionContainer className='bg-light text-dark'>
+        <Link to='/submit/video'
+          style={{textDecoration: 'none', display: 'block'}}
+          className="bg-light">
+          <SubmissionContainer className='text-dark'>
+            <FaVideo size="4em" />
             <h3>Video</h3>
             <p>(YouTube or Vimeo)</p>
           </SubmissionContainer>
         </Link>
       </Col>
       <Col xs='12' md='4'>
-        <Link to='/submit/other' style={{textDecoration: 'none'}}>
-          <SubmissionContainer className='bg-light text-dark'>
+        <Link to='/submit/other'
+          style={{textDecoration: 'none', display: 'block'}}
+          className="bg-light">
+          <SubmissionContainer className='text-dark'>
+            <FaLightbulb size="4em" />
             <h3>Other</h3>
             <p>(eg. A book or physical object)</p>
           </SubmissionContainer>
