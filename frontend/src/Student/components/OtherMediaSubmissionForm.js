8e2abc251bb55b1296692b9106a2f1c81bdc7c6f
Bill Dybas <wmdybas@gmail.com>
2018-01-31 20:23:49 -0500

    Add Initial Other Media Submission Form

--- frontend/src/Student/components/OtherMediaSubmissionForm.js
+++ frontend/src/Student/components/OtherMediaSubmissionForm.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import { Form, FormGroup, FormFeedback, Label, Button, Row, Col } from 'reactstrap'
 import styled from 'styled-components'
 import { Formik, Field } from 'formik'
 import yup from 'yup'
 
+const Header = styled.h1`
+  margin-bottom: 25px;
+`
+
+const ButtonContainer = styled.div`
+  margin-top: 50px;
+`
+
 class OtherSubmissionForm extends Component {
   static propTypes = {
+    user: PropTypes.shape({
+      username: PropTypes.string
+    }).isRequired,
     create: PropTypes.func.isRequired,
     done: PropTypes.func.isRequired
   }
@@ -24,24 +36,52 @@ class OtherSubmissionForm extends Component {
   render () {
     const {
       create,
-      done
+      done,
+      user
     } = this.props
 
     return (
       <Formik
         initialValues={{
-
+          academicProgram: '',
+          yearLevel: '',
+          title: 'Untitled',
+          comment: '',
+          forSale: 'no',
+          moreCopies: '', // TODO: Do we need this for OtherMedia?
+          path: ''
         }}
         validationSchema={
           yup.object()
             .shape({
-
+              academicProgram: yup.string().required('Required'),
+              yearLevel: yup.string().required('Required'),
+              title: yup.string().required('Required'),
+              comment: yup.string(),
+              forSale: yup.string().required('Required').oneOf(['yes', 'no']), // Radio button values
+              moreCopies: yup.string().required('Required').oneOf(['yes', 'no']), // Radio button values
+              path: yup.string().required('Required')
             })}
         onSubmit={(
           values
         ) => {
           const input = {
-
+            entry: {
+              group: {
+                name: '',
+                creatorUsername: '',
+                participants: ''
+              },
+              studentUsername: user.username,
+              showId: 1, // TODO: Update to dynamic
+              academicProgram: values.academicProgram,
+              yearLevel: values.yearLevel,
+              title: values.title,
+              comment: values.comment,
+              forSale: values.forSale === 'yes',
+              moreCopies: values.moreCopies === 'yes'
+            },
+            path: values.path
           }
 
           create(input).then(done())
@@ -56,18 +96,103 @@ class OtherSubmissionForm extends Component {
         }) => (
           <Form onSubmit={handleSubmit}>
             <Row>
-              <Col md='6' xs='12'>
-
+              <Col xs='12' md='8' style={{margin: '0 auto'}}>
+                <Header>New Other Submission</Header>
+                <FormGroup>
+                  <Label>Academic Program</Label>
+                  <Field
+                    type='text'
+                    id='academicProgram'
+                    name='academicProgram'
+                    className='form-control'
+                    required
+                  />
+                  {this.renderErrors(touched, errors, 'academicProgram')}
+                </FormGroup>
+                <FormGroup>
+                  <Label>Year Level</Label>
+                  <Field
+                    type='text'
+                    id='yearLevel'
+                    name='yearLevel'
+                    className='form-control'
+                    required
+                  />
+                  {this.renderErrors(touched, errors, 'yearLevel')}
+                </FormGroup>
+                <FormGroup>
+                  <Label>Title</Label>
+                  <Field
+                    type='text'
+                    id='title'
+                    name='title'
+                    className='form-control'
+                    required
+                  />
+                  {this.renderErrors(touched, errors, 'title')}
+                </FormGroup>
+                <FormGroup>
+                  <Label for='comment'>Artist Comment</Label>
+                  <Field
+                    component='textarea'
+                    id='comment'
+                    name='comment'
+                    className='form-control'
+                    rows={4}
+                  />
+                  {this.renderErrors(touched, errors, 'comment')}
+                </FormGroup>
+                <FormGroup>
+                  <Label>Will you sell your artwork?</Label>
+                  <FormGroup check>
+                    <Label check>
+                      <Field
+                        type='radio'
+                        id='forSale'
+                        name='forSale'
+                        value='no'
+                        required
+                        checked={values.forSale === 'no'}
+                      />
+                      <span className='ml-2'>No, I will not sell my artwork.</span>
+                    </Label>
+                  </FormGroup>
+                  <FormGroup check>
+                    <Label check>
+                      <Field
+                        type='radio'
+                        id='forSale'
+                        name='forSale'
+                        value='yes'
+                        required
+                        checked={values.forSale === 'yes'}
+                      />
+                      <span className='ml-2'>Yes, I will sell my artwork.</span>
+                    </Label>
+                  </FormGroup>
+                  {this.renderErrors(touched, errors, 'forSale')}
+                </FormGroup>
+                <ButtonContainer>
+                  <Link to='/submit'>
+                    <Button
+                      type='button'
+                      color='danger'
+                      style={{cursor: 'pointer', width: '80px'}}
+                    >
+                      Back
+                    </Button>
+                  </Link>
+                  <Button
+                    type='submit'
+                    color='primary'
+                    style={{cursor: 'pointer', float: 'right', width: '80px'}}
+                    disabled={isSubmitting}
+                  >
+                    Submit
+                  </Button>
+                </ButtonContainer>
               </Col>
             </Row>
-            <Button
-              type='submit'
-              color='primary'
-              style={{cursor: 'pointer'}}
-              disabled={isSubmitting}
-            >
-              Submit
-            </Button>
           </Form>
         )}
       />
