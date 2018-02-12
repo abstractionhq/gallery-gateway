b0c97e3e1455e70c187a168410e9e76879483403
Robert McLaughlin <robert@sparkk.us>
2018-02-12 17:03:48 -0500

    change import order of initializes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,22 @@
-// Initialize Global Imports
-import 'moment-timezone' // See: https://github.com/headzoo/react-moment#timezone-support
-import 'react-dates/initialize' // See: https://github.com/airbnb/react-dates#initialize
-import 'bootstrap/dist/css/bootstrap.css' // See: https://github.com/reactstrap/reactstrap#adding-bootstrap
-import 'react-dates/lib/css/_datepicker.css' // See: https://github.com/airbnb/react-dates#webpack
-import 'react-table/react-table.css' // See: https://github.com/react-tools/react-table#installation
-
 import React from 'react'
 import { ApolloProvider } from 'react-apollo'
 import { ConnectedRouter } from 'connected-react-router'
 import { Provider } from 'react-redux'
+
+// Initialize Global Imports
+import 'moment-timezone' // See: https://github.com/headzoo/react-moment#timezone-support
+import 'react-dates/initialize' // See: https://github.com/airbnb/react-dates#initialize
 
 import apollo from '../config/apollo'
 import history from './history'
 import store from './store'
 
 import LoginSwitch from './shared/components/LoginSwitch'
+
+// Import Global CSS
+import 'bootstrap/dist/css/bootstrap.css' // See: https://github.com/reactstrap/reactstrap#adding-bootstrap
+import 'react-dates/lib/css/_datepicker.css' // See: https://github.com/airbnb/react-dates#webpack
+import 'react-table/react-table.css' // See: https://github.com/react-tools/react-table#installation
 
 const Root = () => (
   <ApolloProvider client={apollo}>
