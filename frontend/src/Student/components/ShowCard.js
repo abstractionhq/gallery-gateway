250531706f47cc5f8b0f2b5c7e2eae3a66a7f93d
Tina Howard <chh2538@rit.edu>
2018-02-16 14:17:55 -0500

    Add the student show query and the intial empty card

---
+++ frontend/src/Student/components/ShowCard.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import Moment from 'react-moment'
+import FaPlusCircle from 'babel-loader!react-icons/fa/plus-circle'
+import { Button, ButtonGroup, Row, Col } from 'reactstrap'
+
+const Card = styled.div`
+  background-color: #f8f9fa;
+  border-radius: 5px;
+  margin-top: 15px;
+  padding: 10px;
+  width: 100%;
+`
+
+const ShowCard = (props) => (
+  <Card>
+    <Row>
+      <Col>
+        <h2>{props.name}</h2>
+      </Col>
+      <Col className="text-right">
+      <div>
+        <h5>{props.entries ? props.entries.length : 0}/{props.entryCap} submissions</h5>
+      </div>
+      <div>
+        Submission Ends: <Moment format='YYYY/MM/DD'>{props.entryEnd}</Moment>
+      </div>
+      </Col>
+    </Row>
+    <hr />
+    <Row>
+      <Col className='text-center align-self-center h-100'>
+       <Link to='/submit' className='d-block w-100 h-100'>
+         <FaPlusCircle size='3em' />
+         <h5>New Submission</h5>
+       </Link>
+      </Col>
+    </Row>
+  </Card>
+)
+
+ShowCard.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  entryCap: PropTypes.number.isRequired,
+  entries: PropTypes.array.isRequired,
+  entryStart: PropTypes.string.isRequired,
+  entryEnd: PropTypes.string.isRequired,
+  judgingStart: PropTypes.string.isRequired,
+  judgingEnd: PropTypes.string.isRequired
+}
+
+export default ShowCard
