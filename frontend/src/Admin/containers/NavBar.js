624a1dc7685646354cc642928e245cb6fe234a77
Bill Dybas <wmdybas@gmail.com>
2018-02-21 11:01:57 -0500

    Don't Show "Switch to Admin" to Judges who aren't Admins

--- frontend/src/Admin/containers/NavBar.js
+++ frontend/src/Admin/containers/NavBar.js
@@ -3,12 +3,9 @@ import { connect } from 'react-redux'
 import NavBar from '../components/NavBar'
 import { logout, switchToJudge } from '../../shared/actions'
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    logout: () => dispatch(logout()),
-    switchToJudge: () => dispatch(switchToJudge())
-
-  }
-}
+const mapDispatchToProps = (dispatch) => ({
+  logout: () => dispatch(logout()),
+  switchToJudge: () => dispatch(switchToJudge())
+})
 
 export default connect(null, mapDispatchToProps)(NavBar)
