a763e5d8ec11d8502e41b1a22f59e4ed4996ac02
Robert McLaughlin <robert@sparkk.us>
2018-02-27 18:35:58 -0500

    Add small icons & thumnails for submitted entries
    Adds icons (youtube, vimeo, other ...) and thumbnails
    (photos) on a student's dashboard to show them what
    they have submitted.

--- frontend/src/Student/components/ShowCard.js
+++ frontend/src/Student/components/ShowCard.js
@@ -4,15 +4,60 @@ import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import Moment from 'react-moment'
 import FaPlusCircle from 'react-icons/lib/fa/plus-circle'
-import { Row, Col } from 'reactstrap'
+import FaBook from 'babel-loader!react-icons/fa/book'
+import FaYouTube from 'babel-loader!react-icons/fa/youtube'
+import FaVimeo from 'babel-loader!react-icons/fa/vimeo'
+import { Row, Col, UncontrolledTooltip } from 'reactstrap'
 
 const Card = styled.div`
   background-color: #f8f9fa;
   border-radius: 5px;
-  margin-top: 15px;
+  margin-bottom: 15px;
   padding: 10px;
   width: 100%;
 `
+
+const PhotoThumbnail = styled.img`
+    height: auto;
+    width: auto;
+    min-width: 4em;
+    max-height: 10em;
+    max-width: 100%;
+`
+
+const EntryNoThumbContainer = styled.div`
+  height: 100%;
+  padding: 15px;
+`
+
+const EntryThumb = ({entry}) => {
+  switch (entry.__typename) {
+    case 'Photo':
+      return <PhotoThumbnail
+        src={`//localhost:3000/static/uploads/${entry.path}`}
+      />
+    case 'Video':
+      const url = entry.provider === 'youtube'
+        ? `https://youtube.com/watch?v=${entry.videoId}`
+        : `https://vimeo.com/${entry.videoId}`
+      const icon = entry.provider === 'youtube'
+        ? <FaYouTube size='3em' />
+        : <FaVimeo size='3em' />
+      return <a href={url} target='_blank'>
+        <EntryNoThumbContainer>
+          {icon}
+          <h5>{entry.title}</h5>
+        </EntryNoThumbContainer>
+      </a>
+    case 'OtherMedia':
+      return <EntryNoThumbContainer>
+        <FaBook size='3em' />
+        <h5>{entry.title}</h5>
+      </EntryNoThumbContainer>
+    default:
+      return null
+  }
+}
 
 const ShowCard = (props) => (
   <Card>
@@ -30,9 +75,23 @@ const ShowCard = (props) => (
       </Col>
     </Row>
     <hr />
-    <Row style={{ minHeight: '250px' }}>
-      <Col className='text-center align-self-center justify-content-center h-100'>
-        <Link to={`/submit?to=${props.show.id}`} className='d-block w-100 h-100'>
+    <Row style={{ minHeight: '250px' }} className='align-items-center'>
+      {
+        props.show.entries.map(entry => (
+          <Col
+            md='3'
+            className='text-center align-self-center d-flex justify-content-center align-items-center'
+            style={{minHeight: '10em'}}
+            title={entry.title}
+            key={entry.id}>
+            <EntryThumb entry={entry} />
+          </Col>
+        ))
+      }
+      <Col
+        style={{minHeight: '10em'}}
+        className='text-center align-self-center d-flex justify-content-center align-items-center'>
+        <Link to={`/submit?to=${props.show.id}`}>
           <FaPlusCircle size='3em' />
           <h5 className='mt-1'>New Submission</h5>
         </Link>
@@ -46,7 +105,14 @@ ShowCard.propTypes = {
     id: PropTypes.string,
     name: PropTypes.string,
     entryCap: PropTypes.number,
-    entries: PropTypes.array,
+    entries: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      __typename: PropTypes.oneOf(['Photo', 'Video', 'OtherMedia']).isRequired,
+      path: PropTypes.string,
+      provider: PropTypes.oneOf(['youtube', 'vimeo']).isRequired,
+      videoId: PropTypes.string
+    })),
     entryStart: PropTypes.string,
     entryEnd: PropTypes.string,
     judgingStart: PropTypes.string,
