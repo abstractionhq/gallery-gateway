bb155d6fbcba9377adb4135c75faa8f0f90603eb
Tina Howard <chh2538@rit.edu>
2018-03-05 17:07:19 -0500

    Begin passing the username

--- frontend/src/Student/containers/Shows.js
+++ frontend/src/Student/containers/Shows.js
@@ -1,7 +1,16 @@
 import { graphql } from 'react-apollo'
 
 import ShowsQuery from '../queries/shows.graphql'
+import { connect } from 'react-redux'
 import Shows from '../components/Shows'
+
+const mapStateToProps = state => ({
+  studentUsername: state.shared.auth.user.username
+})
+
+const withRedux = connect(mapStateToProps, null)(
+  Shows
+)
 
 export default graphql(ShowsQuery, {
   props: ({ data: { shows, loading } }) => ({
@@ -13,4 +22,4 @@ export default graphql(ShowsQuery, {
       studentUsername: ownProps.studentUsername
     }
   })
-})(Shows)
+})(withRedux)
