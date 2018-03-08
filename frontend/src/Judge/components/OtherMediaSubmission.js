42f9528291d0fa44b222e91df18b260a53358045
kayladavis <kd9205@rit.edu>
2018-03-08 15:38:05 -0800

    Adding left and right arrow key input for vote navigation (#208)
    * judge arrow input added
    
    * linting

--- frontend/src/Judge/components/OtherMediaSubmission.js
+++ frontend/src/Judge/components/OtherMediaSubmission.js
@@ -1,9 +1,5 @@
 import React from 'react'
 
-const OtherMediaSubmission = props => (
-  <div>
-    Other Submission
-  </div>
-)
+const OtherMediaSubmission = props => <div>Other Submission</div>
 
 export default OtherMediaSubmission
