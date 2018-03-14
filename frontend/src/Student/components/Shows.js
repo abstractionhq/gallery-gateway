d9cae6f2faccce3ad8407d37c3a2f92737d46299
Tina Howard <chh2538@rit.edu>
2018-03-14 17:40:49 -0400

    Add zero state for no students

--- frontend/src/Student/components/Shows.js
+++ frontend/src/Student/components/Shows.js
@@ -1,13 +1,31 @@
-import React from 'react'
 import PropTypes from 'prop-types'
+import React from 'react'
+import styled from 'styled-components'
 
 import ShowCard from '../components/ShowCard'
 
-const Shows = ({ shows, loading }) => (
-  <div>
-    {loading ? null : shows.map(show => <ShowCard key={show.id} show={show} />)}
-  </div>
+const NoShowsContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: large;
+`
+const noShowsView = (
+  <NoShowsContainer>
+    No shows are currently running, come back later
+  </NoShowsContainer>
 )
+
+const Shows = ({ shows, loading }) => {
+  if (loading) return null
+  if (shows.length === 0) return noShowsView
+  return (
+    <div>
+      {shows.map(show => <ShowCard key={show.id} show={show} />)}
+    </div>
+  )
+}
 
 Shows.propTypes = {
   shows: PropTypes.array.isRequired,
