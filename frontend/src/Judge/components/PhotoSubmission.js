80c0ced400c3aa74add32cab4155de6134e92b98
Kayla Davis <kd9205@rit.edu>
2018-03-21 11:50:59 -0700

    image onclick works

--- frontend/src/Judge/components/PhotoSubmission.js
+++ frontend/src/Judge/components/PhotoSubmission.js
@@ -1,23 +1,64 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
+import Lightbox from 'react-images';
 
 
 const PhotoContainer = styled.img`
-width: auto;
-height: 100%;
-text-align:center;
+  width:100%;
+  height:100%; //change to whatever your choice
+
+  /*Scale down will take the necessary specified space that is 40% x 40% without stretching the image*/
+  object-fit:scale-down;
 `
 
-const PhotoSubmission = props =>
-  <PhotoContainer
-    // TODO make this URL responsive to deploy environment
-    src={`//localhost:3000/static/uploads/${props.path}`}
-  >
-  </PhotoContainer>
+class PhotoSubmission extends Component {
+  static propTypes = {
+    path: PropTypes.string.isRequired
+  }
 
-PhotoSubmission.propTypes = {
-  path: PropTypes.string.isRequired
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      lightboxIsOpen: false,
+    }
+  }
+
+  closeLightbox = () => {
+    this.setState({ lightboxIsOpen: false })
+  }
+
+  openLightbox = () => {
+    this.setState({ lightboxIsOpen: true })
+  }
+
+
+  render() {
+    const {
+      path
+    } = this.props
+
+    return (
+      <Fragment>
+      <PhotoContainer
+        // TODO make this URL responsive to deploy environment
+        src={`//localhost:3000/static/uploads/${path}`}
+        onClick={() => this.openLightbox()}
+      >
+      </PhotoContainer>
+      <Lightbox
+        images={[{ src: `//localhost:3000/static/uploads/${path}` }]}
+        isOpen={this.state.lightboxIsOpen}
+        onClose={() => this.closeLightbox()}
+        showImageCount={false}
+        backdropClosesModal={true}
+        onClickImage={() => this.closeLightbox()}
+      />  
+      </Fragment>
+    )
+  }
 }
+
 
 export default PhotoSubmission
