a76bc47ba6f259d41681888062962626e4b0507d
Tina Howard <chh2538@rit.edu>
2018-03-27 20:30:18 -0400

    Add loading icon as a component and to shows

--- frontend/src/Admin/components/Shows.js
+++ frontend/src/Admin/components/Shows.js
@@ -2,10 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import ShowCard from '../components/ShowCard'
+import LoadingIcon from '../components/Loading'
 
 const Shows = ({ shows, loading }) => (
   <div>
-    {loading || !shows ? null : shows.map(show => <ShowCard key={show.id} {...show} />)}
+    {loading || !shows ? <LoadingIcon /> : shows.map(show => <ShowCard key={show.id} {...show} />)}
   </div>
 )
 
