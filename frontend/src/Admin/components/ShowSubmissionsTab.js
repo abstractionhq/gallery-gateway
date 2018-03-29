1ad14441b49388cefa9c3cc1fcaf7ad2744023f4
Kayla Davis <kd9205@rit.edu>
2018-03-28 17:04:16 -0700

    addng alert

--- frontend/src/Admin/components/ShowSubmissionsTab.js
+++ frontend/src/Admin/components/ShowSubmissionsTab.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { Alert } from 'reactstrap'
 import ReactTable from 'react-table'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
@@ -9,6 +10,10 @@ import FaStarOpen from 'react-icons/lib/fa/star-o'
 import FaBook from 'react-icons/lib/fa/book'
 
 import { getImageThumbnail } from '../../utils'
+
+// Alert Types
+const SUCCESS = 'SUCCESS'
+const ERROR = 'ERROR'
 
 const PhotoThumbnail = styled.img`
   height: auto;
@@ -39,132 +44,174 @@ class ShowSubmissionsTab extends Component {
     super(props)
 
     this.state = {
-      alertVisible: false,
+      alertVisible: true,
       alertType: ''
     }
+  }
+
+  onDismiss = () => {
+    this.setState({
+      alertVisible: true,
+      alertType: ''
+    })
   }
 
   updateShowInvite = (id, value) => {
     const { updateInvite } = this.props
     updateInvite(id, value)
       .then(() => {
-        console.log('updated!!!')
+        this.setState({
+          alertVisible: true,
+          alertType: SUCCESS
+        })
+        setTimeout(() => {
+          this.onDismiss()
+        }, 3000)
+      })
+      .catch(() => {
+        this.setState({
+          alertVisible: true,
+          alertType: ERROR
+        })
+        setTimeout(() => {
+          this.onDismiss()
+        }, 3000)
       })
   }
 
-  render () {
+  render() {
     const { show } = this.props
 
     return (
-      <ReactTable
-      defaultPageSize={20}
-      data={show.entries}
-      columns={[
-        {
-          Header: 'Row',
-          Cell: row => row.viewIndex + 1,
-          maxWidth: 50,
-          sortable: false
-        },
-        {
-          Header: 'Thumbnail',
-          Cell: ({ original: submission }) => {
-            switch (submission.entryType) {
-              case 'PHOTO':
-                return (
-                  <PhotoThumbnail
-                    alt={submission.title}
-                    src={`//localhost:3000/static/uploads/${getImageThumbnail(
-                      submission.path
-                    )}`}
-                  />
-                )
-              case 'VIDEO':
-                if (submission.provider === 'youtube') {
-                  return <FaYouTube size='2em' />
-                } else {
-                  return <FaVimeo size='2em' />
-                }
-              case 'OTHER':
-                return <FaBook size='2em' />
-              default:
-                console.log(submission.entryType)
-                return null
-            }
+      <Fragment>
+        <Alert
+          color='success'
+          style={{position: 'fixed', bottom: '0', left: '0', width: '100%', zIndex: '5'}}
+          isOpen={this.state.alertVisible && this.state.alertType === SUCCESS}
+          toggle={() => this.onDismiss()}
+          className='text-center'
+        >
+          Invite status saved
+        </Alert>
+        <Alert
+          color='danger'
+          style={{position: 'fixed', bottom: '0', left: '0', width: '100%', zIndex: '5'}}
+          isOpen={this.state.alertVisible && this.state.alertType === ERROR}
+          toggle={() => this.onDismiss()}
+          className='text-center'
+        >
+          There was an error updating the invite status
+        </Alert>
+        <ReactTable
+        defaultPageSize={20}
+        data={show.entries}
+        columns={[
+          {
+            Header: 'Row',
+            Cell: row => row.viewIndex + 1,
+            maxWidth: 50,
+            sortable: false
           },
-          sortable: false,
-          style: { textAlign: 'center' },
-          width: 150
-        },
-        {
-          Header: 'Title',
-          accessor: 'title'
-        },
-        {
-          id: 'artist',
-          Header: 'Artist',
-          accessor: submission => {
-            // Allows for sorting by student submitter's name
-            const student = submission.student || submission.group.creator
-            return `${student.lastName}${student.firstName}${student.username}`
+          {
+            Header: 'Thumbnail',
+            Cell: ({ original: submission }) => {
+              switch (submission.entryType) {
+                case 'PHOTO':
+                  return (
+                    <PhotoThumbnail
+                      alt={submission.title}
+                      src={`//localhost:3000/static/uploads/${getImageThumbnail(
+                        submission.path
+                      )}`}
+                    />
+                  )
+                case 'VIDEO':
+                  if (submission.provider === 'youtube') {
+                    return <FaYouTube size='2em' />
+                  } else {
+                    return <FaVimeo size='2em' />
+                  }
+                case 'OTHER':
+                  return <FaBook size='2em' />
+                default:
+                  console.log(submission.entryType)
+                  return null
+              }
+            },
+            sortable: false,
+            style: { textAlign: 'center' },
+            width: 150
           },
-          Cell: ({ original: submission }) =>
-            !submission.group ? (
-              `${submission.student.lastName}, ${
-              submission.student.firstName
-              } (${submission.student.username})`
-            ) : (
-                <div>
-                  <small>Group</small>
-                  <p>
-                    {submission.group.creator.lastName},{' '}
-                    {submission.group.creator.firstName} ({
-                      submission.group.creator.username
-                    })
+          {
+            Header: 'Title',
+            accessor: 'title'
+          },
+          {
+            id: 'artist',
+            Header: 'Artist',
+            accessor: submission => {
+              // Allows for sorting by student submitter's name
+              const student = submission.student || submission.group.creator
+              return `${student.lastName}${student.firstName}${student.username}`
+            },
+            Cell: ({ original: submission }) =>
+              !submission.group ? (
+                `${submission.student.lastName}, ${
+                submission.student.firstName
+                } (${submission.student.username})`
+              ) : (
+                  <div>
+                    <small>Group</small>
+                    <p>
+                      {submission.group.creator.lastName},{' '}
+                      {submission.group.creator.firstName} ({
+                        submission.group.creator.username
+                      })
                 </p>
-                  <p>Participants: {submission.group.participants}</p>
-                </div>
-              )
-        },
-        {
-          id: 'dimensions',
-          accessor: submission =>
-            // Allows for sorting by area
-            submission.entryType === 'PHOTO'
-              ? submission.horizDimInch * submission.vertDimInch
-              : 0,
-          Header: 'Dimensions',
-          Cell: ({ original: submission }) =>
-            submission.entryType === 'PHOTO'
-              ? `${submission.horizDimInch} in. \u00D7 ${
-              submission.vertDimInch
-              } in.`
-              : 'n/a'
-        },
-        {
-          Header: 'Score',
-          accessor: 'score'
-        },
-        {
-          Header: 'Invited',
-          accessor: 'invited',
-          Cell: ({ original: submission }) =>
-            submission.invited ? (
-              <a style={{ cursor: 'pointer' }}
-              onClick={() => this.updateShowInvite(submission.id, false)}>
-                <FaStar size='1.5em' style={{ color: 'gold' }} />
-              </a>
-            ) : (
+                    <p>Participants: {submission.group.participants}</p>
+                  </div>
+                )
+          },
+          {
+            id: 'dimensions',
+            accessor: submission =>
+              // Allows for sorting by area
+              submission.entryType === 'PHOTO'
+                ? submission.horizDimInch * submission.vertDimInch
+                : 0,
+            Header: 'Dimensions',
+            Cell: ({ original: submission }) =>
+              submission.entryType === 'PHOTO'
+                ? `${submission.horizDimInch} in. \u00D7 ${
+                submission.vertDimInch
+                } in.`
+                : 'n/a'
+          },
+          {
+            Header: 'Score',
+            accessor: 'score'
+          },
+          {
+            Header: 'Invited',
+            accessor: 'invited',
+            Cell: ({ original: submission }) =>
+              submission.invited ? (
                 <a style={{ cursor: 'pointer' }}
-                onClick={() => this.updateShowInvite(submission.id, true)}>
-                  <FaStarOpen size='1.5em' />
+                  onClick={() => this.updateShowInvite(submission.id, false)}>
+                  <FaStar size='1.5em' style={{ color: 'gold' }} />
                 </a>
-              ),
-          style: { textAlign: 'center' },
-          width: 80
-        }
-      ]}
-    />
+              ) : (
+                  <a style={{ cursor: 'pointer' }}
+                    onClick={() => this.updateShowInvite(submission.id, true)}>
+                    <FaStarOpen size='1.5em' />
+                  </a>
+                ),
+            style: { textAlign: 'center' },
+            width: 80
+          }
+        ]}
+      />
+      </Fragment>
     )
   }
 }
