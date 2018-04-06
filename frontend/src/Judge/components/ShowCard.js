362f36a7f2f889dde58354779907f1570e670b01
Bill Dybas <wmdybas@gmail.com>
2018-04-06 00:08:42 -0400

    Remove Unused Imports

--- frontend/src/Judge/components/ShowCard.js
+++ frontend/src/Judge/components/ShowCard.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import Moment from 'react-moment'
-import { Button, Row, Col } from 'reactstrap'
+import { Button, Col } from 'reactstrap'
 import moment from 'moment'
 
 const Card = styled.div`
