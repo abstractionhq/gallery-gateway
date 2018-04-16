bbea0aadf129487e5d04f767af6b4b31bf11b46e
Robert McLaughlin <robert@sparkk.us>
2018-04-15 22:15:54 -0400

    add warning modal when unassigning judges

--- frontend/src/Admin/components/AssignJudgesTable.js
+++ frontend/src/Admin/components/AssignJudgesTable.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react'
-import { Row, Col, Button } from 'reactstrap'
+import React, { Component, Fragment } from 'react'
+import {
+  Row,
+  Col,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter
+} from 'reactstrap'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import FaLongArrowLeft from 'react-icons/lib/fa/long-arrow-left'
 import FaLongArrowRight from 'react-icons/lib/fa/long-arrow-right'
+import FaExclamationTriangle from 'react-icons/lib/fa/exclamation-triangle'
 
 import JudgesTable from '../components/JudgesTable'
 
@@ -38,11 +47,24 @@ class AssignJudgesTable extends Component {
 
   state = {
     selectedUnassignedJudges: {},
-    selectedAssignedJudges: {}
+    selectedAssignedJudges: {},
+    isUnassignConfirmationOpen: false
   }
 
   componentDidMount () {
     this.props.fetchData()
+  }
+
+  onDismissUnassignConfirmation () {
+    this.setState({
+      isUnassignConfirmationOpen: false
+    })
+  }
+
+  onDisplayUnassignConfirmation () {
+    this.setState({
+      isUnassignConfirmationOpen: true
+    })
   }
 
   assign = () => {
@@ -87,52 +109,86 @@ class AssignJudgesTable extends Component {
     const { data } = this.props
 
     return (
-      <Row>
-        <Col xs='12' md='5'>
-          <CenteredSubHeading>Unassigned</CenteredSubHeading>
-          <JudgesTable
-            judges={data.unassignedJudges}
-            selected={this.state.selectedUnassignedJudges}
-            onChange={this.handleUnassignedJudgesChange}
-          />
-        </Col>
-        <Col xs='12' md='2' className='align-self-center'>
-          <Row>
-            <Col xs={12}>
-              <ReassignButtonContainer>
-                <Button
-                  color='primary'
-                  block
-                  style={{ cursor: 'pointer' }}
-                  onClick={() => this.assign()}
-                >
-                  Assign <FaLongArrowRight />
-                </Button>
-              </ReassignButtonContainer>
-            </Col>
-            <Col xs={12}>
-              <ReassignButtonContainer>
-                <Button
-                  color='primary'
-                  block
-                  style={{ cursor: 'pointer' }}
-                  onClick={() => this.unassign()}
-                >
-                  <FaLongArrowLeft /> Unassign
-                </Button>
-              </ReassignButtonContainer>
-            </Col>
-          </Row>
-        </Col>
-        <Col xs='12' md='5'>
-          <CenteredSubHeading>Assigned</CenteredSubHeading>
-          <JudgesTable
-            judges={data.assignedJudges}
-            selected={this.state.selectedAssignedJudges}
-            onChange={this.handleAssignedJudgesChange}
-          />
-        </Col>
-      </Row>
+      <Fragment>
+        <Modal
+          isOpen={this.state.isUnassignConfirmationOpen}
+          toggle={this.onDismissUnassignConfirmation}
+          style={{ minWidth: '50%' }}
+        >
+          <ModalHeader toggle={this.onDismissUnassignConfirmation}>
+            Warning <FaExclamationTriangle />
+          </ModalHeader>
+          <ModalBody>
+            <p>
+              Removing a judge will permanently delete any votes they have made
+              in this show.
+            </p>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              color='secondary'
+              onClick={() => this.onDismissUnassignConfirmation()}
+            >
+              Cancel
+            </Button>{' '}
+            <Button
+              color='danger'
+              onClick={() => {
+                this.onDismissUnassignConfirmation()
+                this.unassign()
+              }}
+            >
+              Continue
+            </Button>
+          </ModalFooter>
+        </Modal>
+        <Row>
+          <Col xs='12' md='5'>
+            <CenteredSubHeading>Unassigned</CenteredSubHeading>
+            <JudgesTable
+              judges={data.unassignedJudges}
+              selected={this.state.selectedUnassignedJudges}
+              onChange={this.handleUnassignedJudgesChange}
+            />
+          </Col>
+          <Col xs='12' md='2' className='align-self-center'>
+            <Row>
+              <Col xs={12}>
+                <ReassignButtonContainer>
+                  <Button
+                    color='primary'
+                    block
+                    style={{ cursor: 'pointer' }}
+                    onClick={() => this.assign()}
+                  >
+                    Assign <FaLongArrowRight />
+                  </Button>
+                </ReassignButtonContainer>
+              </Col>
+              <Col xs={12}>
+                <ReassignButtonContainer>
+                  <Button
+                    color='primary'
+                    block
+                    style={{ cursor: 'pointer' }}
+                    onClick={() => this.onDisplayUnassignConfirmation()}
+                  >
+                    <FaLongArrowLeft /> Unassign
+                  </Button>
+                </ReassignButtonContainer>
+              </Col>
+            </Row>
+          </Col>
+          <Col xs='12' md='5'>
+            <CenteredSubHeading>Assigned</CenteredSubHeading>
+            <JudgesTable
+              judges={data.assignedJudges}
+              selected={this.state.selectedAssignedJudges}
+              onChange={this.handleAssignedJudgesChange}
+            />
+          </Col>
+        </Row>
+      </Fragment>
     )
   }
 }
