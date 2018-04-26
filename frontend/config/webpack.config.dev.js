8390b66098577b515e3fdc755bdee7c3a0aee2f3
Bill Dybas <wmdybas@gmail.com>
2018-04-26 01:57:47 -0400

    Update Webpack Configs
    - Add comments
    - Group larger packages into similar chunks
    - When building for production, remove existing 'dist' folder
    - Ignore moment locale files since they're quite large and we don't need them
    - Remove unnecessary 'runtime' chunk

--- frontend/config/webpack.config.dev.js
+++ frontend/config/webpack.config.dev.js
@@ -1,40 +1,73 @@
+const path = require('path')
+
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
-const path = require('path')
 
 module.exports = {
   entry: {
-    main: [
+    Main: [
       'react-hot-loader/patch',
       'webpack-dev-server/client?http://localhost:5000',
       'webpack/hot/only-dev-server',
       './src/app.js'
     ],
-    vendor: [
+    Lib: [
+      'formik',
+      'yup',
+      'moment',
+      'react-dates',
+      'react-dropzone',
+      'react-images',
+      'react-moment',
+      'react-select',
+      'react-table',
+      'redux',
+      'react-redux',
+      'redux-thunk',
+      'recompose'
+    ],
+    React: [
       'react',
-      'react-dom'
+      'react-dom',
+      'react-router',
+      'react-router-dom',
+      'connected-react-router',
+      'react-router-tabs'
+    ],
+    GQL: [
+      'apollo-client',
+      'apollo-client-preset',
+      'graphql',
+      'graphql-tag',
+      'react-apollo'
     ]
   },
   output: {
-    path: __dirname, // TODO: May need to change
+    path: __dirname,
     filename: '[name].bundle.js',
     publicPath: '/'
   },
   resolve: {
-    extensions: ['.js'],
+    extensions: ['.js'], // Matches any file with a '.js' extension
     modules: [
+      // Looks for files in './src' and 'node_modules'
       path.resolve('./src'),
       'node_modules'
     ]
   },
   devtool: 'cheap-module-source-map',
   plugins: [
+    // Allows for hot reloading
     new webpack.HotModuleReplacementPlugin(),
+    // Causes the relative path of the module to be displayed when HMR is enabled
     new webpack.NamedModulesPlugin(),
+    // Skips the emitting phase whenever there are errors while compiling
     new webpack.NoEmitOnErrorsPlugin(),
+    // Set 'process.env.NODE_ENV' to 'development'
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development')
     }),
+    // Compiles our template
     new HtmlWebpackPlugin({
       title: 'Gallery Gateway',
       template: './src/index.ejs'
