d1f2a0b0d6fd6faa998712fa088d0154fb42db68
Robert McLaughlin <robert@sparkk.us>
2018-04-30 15:48:11 -0400

    fix vertical separator in navbar being in link

--- frontend/src/Admin/components/NavBar.js
+++ frontend/src/Admin/components/NavBar.js
@@ -51,10 +51,17 @@ export default class NavBar extends Component {
             <Nav className='ml-auto' navbar>
               <NavItem>
                 <NavLink tag={Link} to='/' onClick={this.props.switchToJudge}>
-                  View as Judge |
+                  View as Judge
                 </NavLink>
-                {/* TODO: Move the | out of the link */}
               </NavItem>
+              <li>
+                <span
+                  className='text-muted d-md-block d-none'
+                  style={{ padding: '8px', cursor: 'default' }}
+                >
+                  |
+                </span>
+              </li>
               <NavItem>
                 <NavLink tag={Link} to='/judges'>
                   Judges
