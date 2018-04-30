77b7fa00b4524956cd2aa2d3cc77de4bab3a97cf
Bill Dybas <wmdybas@gmail.com>
2018-04-30 18:03:54 -0400

    Comments & Backend Security
    - Disable X-Powered-By header
    - Only use CORS in development

--- backend/server.js
+++ backend/server.js
@@ -14,26 +14,46 @@ import parseJwtUser from './middleware/parseJwtUser'
 
 const app = express()
 
+// Setup Model Associations
 models()
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+// Disable 'X-Powered-By' Header (See: https://expressjs.com/en/advanced/best-practice-security.html)
+app.disable('x-powered-by')
+
+// When not in production, accept requests from any origin since we'll
+// be running the frontend and backend on separate ports. In production,
+// we use nginx to serve them at the same origin, so CORS is not necessary.
+if (config.get('NODE_ENV') !== 'production') {
+  app.use(cors())
+}
+
+app.use(bodyParser.json()) // Parse application/json
+app.use(bodyParser.urlencoded({ extended: false })) // Parse application/x-www-form-urlencoded
+
+// Initialize Passport - for SAML Auth
 app.use(passport.initialize())
+
+// Add JWT parsing middleware
 app.use(parseJwtUser)
+
+// Setup Routes
 app.use(router)
 
+// Don't expose GraphQL Internal Errors to the Client
 maskErrors(schema)
+// Setup GraphQL endpoint
 app.use('/graphql', graphqlExpress(req => ({
   schema,
   context: req
 })))
 
+// Setup GraphiQL web interface in development
 if (config.get('NODE_ENV') !== 'production') {
   app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
 }
 
+// Setup static asset serving
 app.use('/static/uploads', express.static(path.join(__dirname, 'uploads', 'images')))
 app.use('/static/uploads', express.static(path.join(__dirname, 'uploads', 'pdfs')))
 
-export default app // for testing
+export default app
