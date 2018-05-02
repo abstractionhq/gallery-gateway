d5d5c34973b75c12fa6cdc81cf2161270c3ed72e
Bill Dybas <wmdybas@gmail.com>
2018-05-02 12:08:46 -0400

    Refactor ShowCard
    Deduplicates markup, identifying
    that we only change the sub-heading
    and during submissions show how many
    of each type have been submitted

--- frontend/src/Admin/components/ShowCard.js
+++ frontend/src/Admin/components/ShowCard.js
@@ -1,15 +1,14 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-import { Row, Col, Button } from 'reactstrap'
+import { Button, Row, Col, Badge } from 'reactstrap'
 import moment from 'moment'
 import Moment from 'react-moment'
-import BeforeSubmission from './BeforeSubmission'
-import DuringSubmission from './DuringSubmission'
-import DuringJudging from './DuringJudging'
-import AfterShowEnd from './AfterShowEnd'
-import EntrySummary from './EntrySummary'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import FaImage from '@fortawesome/fontawesome-free-regular/faImage'
+import FaVideo from '@fortawesome/fontawesome-free-solid/faVideo'
+import FaBook from '@fortawesome/fontawesome-free-solid/faBook'
 
 const Card = styled.div`
   background-color: #f8f9fa;
@@ -19,143 +18,217 @@ const Card = styled.div`
   width: 100%;
 `
 
-const ShowCard = props => {
-  const cardBody = (() => {
-    const currentTime = moment()
-    const isAfterEntryStart = currentTime.isAfter(moment(props.entryStart))
-    const isBeforeEntryEnd = currentTime.isBefore(moment(props.entryEnd))
-    const isAfterJudgingStart = currentTime.isAfter(moment(props.judgingStart))
-    const isBeforeJudgingEnd = currentTime.isBefore(moment(props.judgingEnd))
-    const isAfterJudgingEnd = currentTime.isAfter(moment(props.judgingEnd))
-    const isAfterEntryEnd = currentTime.isAfter(moment(props.entryEnd))
-    const isBeforeJudgingStart = currentTime.isBefore(moment(props.judgingStart))
-    // Closed
-    if (isAfterJudgingEnd) {
-      return <AfterShowEnd entryStart={props.entryStart} judgingEnd={props.judgingEnd}/>
-    }
-    // Judging
-    if (isAfterJudgingStart && isBeforeJudgingEnd) {
-      return <DuringJudging judgingEnd={props.judgingEnd}/>
-    }
-    // Between Submission and Judging
-    if (isAfterEntryEnd && isBeforeJudgingStart) {
-      return (
-        <Row>
-          <Col>
-            <dt>Submissions Begin:</dt>
-            <dd>
-              <Moment format='MMMM D, YYYY'>{props.entryStart}</Moment>
-            </dd>
-            <dt>Submission Closes:</dt>
-            <dd>
-              <Moment format='MMMM D, YYYY'>{props.entryEnd}</Moment>
-            </dd>
-          </Col>
-          <Col>
-            <dt>Judging Begins:</dt>
-            <dd>
-              <Moment format='MMMM D, YYYY'>{props.judgingStart}</Moment>
-            </dd>
-            <dt>Judging Closes:</dt>
-            <dd>
-              <Moment format='MMMM D, YYYY'>{props.judgingEnd}</Moment>
-            </dd>
-          </Col>
-        </Row>
-      )
-    }
-    // Submission
-    if (isAfterEntryStart && isBeforeEntryEnd) {
-      return (
-        <div>
-          <DuringSubmission entryStart={props.entryStart} entryEnd={props.entryEnd}/>
-          <EntrySummary
-            totalEntries={props.entries.length}
-            totalPhotos={props.entries.filter(entry => entry.entryType === 'PHOTO').length}
-            totalVideos={props.entries.filter(entry => entry.entryType === 'VIDEO').length}
-            totalOther={props.entries.filter(entry => entry.entryType === 'OTHER').length}
-          />
-        </div>
-      )
-    }
-    // PreSubmission
-    return (
-      <BeforeSubmission
-        entryStart={props.entryStart} entryEnd={props.entryEnd}
-        judgingStart={props.judgingStart} judgingEnd={props.judgingEnd}
-      />
+// NOTE: Only 1 child should be provided
+const FormattedDate = (props) => (
+  <Moment
+    title={moment(props.children).format('MMMM D, YYYY hh:mm:ss a')}
+    format='MMMM D, YYYY'
+  >
+    {props.children}
+  </Moment>
+)
+
+class ShowCard extends Component {
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    entryStart: PropTypes.string.isRequired,
+    entryEnd: PropTypes.string.isRequired,
+    judgingStart: PropTypes.string.isRequired,
+    judgingEnd: PropTypes.string.isRequired,
+    entries: PropTypes.arrayOf(
+      PropTypes.shape({
+        entryType: PropTypes.string.isRequired
+      })
     )
-  })()
-  return (
-    <Card>
-      <h2>
-        <Link to={`show/${props.id}`}>{props.name}</Link>
-      </h2>
-      {cardBody}
+  }
+
+  renderOpenClose = (opens, closes) => (
+    <Fragment>
+      <dl style={{width: '50%'}} className='d-inline-block'>
+        <dt>{moment().isBefore(moment(opens)) ? 'Opens:' : 'Opened:' }</dt>
+        <dd><FormattedDate>{opens}</FormattedDate></dd>
+      </dl>
+      <dl style={{width: '50%'}} className='d-inline-block'>
+        <dt>{moment().isBefore(moment(closes)) ? 'Closes:' : 'Closed:' }</dt>
+        <dd><FormattedDate>{closes}</FormattedDate></dd>
+      </dl>
+    </Fragment>
+  )
+
+  renderSubmissionSummary = (props) => {
+    const totalSubmissions = props.entries.length
+    const totalPhotoSubmissions = props.entries.filter(entry => entry.entryType === 'PHOTO').length
+    const totalVideoSubmissions = props.entries.filter(entry => entry.entryType === 'VIDEO').length
+    const totalOtherSubmissions = props.entries.filter(entry => entry.entryType === 'OTHER').length
+
+    return (
+      <Fragment>
+        <h4>Submissions: <Badge color='primary'>{totalSubmissions}</Badge></h4>
+        <div>
+          <FontAwesomeIcon icon={FaImage} className='mr-2' />
+          <h6 className='text-bold d-inline-block'>
+            Photos: <Badge color='dark'>{totalPhotoSubmissions}</Badge>
+          </h6>
+        </div>
+        <div>
+          <FontAwesomeIcon icon={FaVideo} className='mr-2' />
+          <h6 className='text-bold d-inline-block'>
+            Videos: <Badge color='dark'>{totalVideoSubmissions}</Badge>
+          </h6>
+        </div>
+        <div>
+          <FontAwesomeIcon icon={FaBook} className='mr-2' />
+          <h6 className='text-bold d-inline-block'>
+            Other: <Badge color='dark'>{totalOtherSubmissions}</Badge>
+          </h6>
+        </div>
+      </Fragment>
+    )
+  }
+
+  renderClosedShow = ({ entryStart, judgingEnd }) => (
+    <Row>
+      <Col className='mb-3'>
+        <h4>Closed Show</h4>
+        <FormattedDate>{entryStart}</FormattedDate>
+        {' - '}
+        <FormattedDate>{judgingEnd}</FormattedDate>
+      </Col>
+    </Row>
+  )
+
+  renderBody = (props) => {
+    const {
+      entryStart,
+      entryEnd,
+      judgingStart,
+      judgingEnd
+    } = props
+
+    const now = moment()
+    const isShowInFuture = now.isBefore(moment(entryStart))
+    const isShowInSubmission = now.isAfter(moment(entryStart)) && now.isBefore(moment(entryEnd))
+    const isShowBetweenSubmissionAndJudging = now.isAfter(moment(entryEnd)) && now.isBefore(moment(judgingStart))
+    const isShowInJudging = now.isAfter(moment(judgingStart)) && now.isBefore(moment(judgingEnd))
+    const isShowClosed = now.isAfter(moment(judgingEnd))
+
+    if (isShowClosed) {
+      return this.renderClosedShow(props)
+    }
+
+    // Dynamically set the sub-heading based on the show state
+    let subHeading1 = ''
+    let subHeading2 = ''
+
+    if (isShowInFuture) {
+      subHeading1 = 'Pre Show'
+      subHeading2 = 'Pre Judging'
+    } else if (isShowInSubmission) {
+      subHeading1 = 'Accepting Submissions'
+      subHeading2 = 'Pre Judging'
+    } else if (isShowBetweenSubmissionAndJudging) {
+      subHeading1 = 'No Longer Accepting Submissions'
+      subHeading2 = 'Pre Judging'
+    } else if (isShowInJudging) {
+      subHeading1 = 'No Longer Accepting Submissions'
+      subHeading2 = 'Judging In Progress'
+    }
+
+    // We render Submission Period and Judging Period in two columns,
+    // except for when the show is in submission, we render the summary info
+    // in the second column and move the judging info below the submission period info
+    return (
       <Row>
-        <Col>
-          <Button
-            color='primary'
-            className='mr-4'
-            style={{ cursor: 'pointer' }}
-            tag={Link}
-            to={`/show/${props.id}`}
-            block
-            outline
-          >
-            View Details
-          </Button>
-          <Button
-            color='primary'
-            style={{ cursor: 'pointer' }}
-            tag={Link}
-            to={`/show/${props.id}/submissions`}
-            block
-            outline
-          >
-            View Submissions
-          </Button>
+        <Col xs='12' md='6'>
+          <h4>Submission Period</h4>
+          <h6>{subHeading1}</h6>
+          {this.renderOpenClose(entryStart, entryEnd)}
+          {isShowInSubmission ? (
+            <Fragment>
+              <h4>Judging Period</h4>
+              <h6>{subHeading2}</h6>
+              {this.renderOpenClose(judgingStart, judgingEnd)}
+            </Fragment>
+          ) : null}
         </Col>
-        <Col>
-          <Button
-            className='mr-4'
-            color='primary'
-            style={{ cursor: 'pointer' }}
-            tag={Link}
-            to={`/show/${props.id}/judges`}
-            block
-            outline
-          >
-            View Progress
-          </Button>
-          <Button
-            color='primary'
-            style={{ cursor: 'pointer' }}
-            tag={Link}
-            to={`/show/${props.id}/judges/assign`}
-            block
-            outline
-          >
-            Assign Judges
-          </Button>
+        <Col xs='12' md='6'>
+          {isShowInSubmission
+            ? this.renderSubmissionSummary(props)
+            : (
+              <Fragment>
+                <h4>Judging Period</h4>
+                <h6>{subHeading2}</h6>
+                {this.renderOpenClose(judgingStart, judgingEnd)}
+              </Fragment>
+            )}
         </Col>
       </Row>
-    </Card>
-  )
-}
+    )
+  }
 
-ShowCard.propTypes = {
-  id: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  entryStart: PropTypes.string.isRequired,
-  entryEnd: PropTypes.string.isRequired,
-  judgingStart: PropTypes.string.isRequired,
-  judgingEnd: PropTypes.string.isRequired,
-  entries: PropTypes.arrayOf(
-    PropTypes.shape({
-      entryType: PropTypes.string.isRequired
-    })
+  renderButtons = (props) => (
+    <Row>
+      <Col>
+        <Button
+          color='primary'
+          className='mr-4'
+          style={{ cursor: 'pointer' }}
+          tag={Link}
+          to={`/show/${props.id}`}
+          block
+          outline
+        >
+          View Details
+        </Button>
+        <Button
+          color='primary'
+          style={{ cursor: 'pointer' }}
+          tag={Link}
+          to={`/show/${props.id}/submissions`}
+          block
+          outline
+        >
+          View Submissions
+        </Button>
+      </Col>
+      <Col>
+        <Button
+          className='mr-4'
+          color='primary'
+          style={{ cursor: 'pointer' }}
+          tag={Link}
+          to={`/show/${props.id}/judges`}
+          block
+          outline
+        >
+          View Progress
+        </Button>
+        <Button
+          color='primary'
+          style={{ cursor: 'pointer' }}
+          tag={Link}
+          to={`/show/${props.id}/judges/assign`}
+          block
+          outline
+        >
+          Assign Judges
+        </Button>
+      </Col>
+    </Row>
   )
+
+  render () {
+    return (
+      <Card>
+        <h2>
+          <Link to={`show/${this.props.id}`}>{this.props.name}</Link>
+        </h2>
+        {this.renderBody(this.props)}
+        {this.renderButtons(this.props)}
+      </Card>
+    )
+  }
 }
 
 export default ShowCard
