cbf92e5e39ae0a5943fafdfed62d25eddc9c1402
John Byrne <John Byrne>
2020-03-01 16:56:24 -0500

    start making test

--- backend/test/resolvers/portfolioPeriodQuery.js
+++ backend/test/resolvers/portfolioPeriodQuery.js
@@ -1,9 +1,11 @@
 import { expect } from "chai";
 
 import { openPortfolioPeriod } from "../../resolvers/queries/portfolioPeriodQuery";
+import { assignToPortfolioPeriod } from "../../resolvers/mutations/portfolioPeriod";
 import { fakeUser, fakePortfolioPeriod, fakePortfolio } from "../factories";
+import PortfolioPeriod from "../../models/portfolioPeriod";
 
-describe("Portfolio Period Queries", function() {
+describe("Portfolio Period Queries and Mutators", function() {
   describe("openPortfolioPeriod", function() {
     it("returns null when the student already has a portfolio", function() {
       return Promise.all([fakeUser(), fakePortfolioPeriod()])
@@ -41,6 +43,26 @@ describe("Portfolio Period Queries", function() {
         .then(period => {
           return expect(period).to.be.not.null;
         });
+    });
+  });
+  describe("Adding and removing judges from the portfolio Period", () => {
+    it("adds judges to a portfolio period", () => {
+      return Promise.all(fakeUser({ type: "JUDGE" }), fakePortfolioPeriod())
+        .then(([user, portfolioPeriod]) =>
+          Promise.all([
+            portfolioPeriod,
+            assignToPortfolioPeriod(
+              {},
+              {
+                portfolioPeriodId: portfolioPeriod.id,
+                usernames: [user.username]
+              },
+              { auth: { type: "ADMIN" } }
+            )
+          ])
+        )
+        .then(([portfolioPeriod]) => portfolioPeriod.getUsers())
+        .then(users => expect(users.length).to.equal(1));
     });
   });
 });
