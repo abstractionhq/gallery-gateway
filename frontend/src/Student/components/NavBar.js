f19b03bea6346cc8faae3620126d17881c610c5c
John P. Murray <johnphilmurray@gmail.com>
2020-03-18 11:27:20 -0400

    updated nav bar

--- frontend/src/Student/components/NavBar.js
+++ frontend/src/Student/components/NavBar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-import { Collapse, Navbar, NavbarToggler, Nav, NavItem } from 'reactstrap'
+import { Collapse, Navbar, NavbarToggler, Nav, NavItem, NavLink } from 'reactstrap'
 import styled from 'styled-components'
 
 const NavBarContainer = styled.div`
@@ -37,6 +37,24 @@ export default class NavBar extends Component {
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className='ml-auto' navbar>
+              <NavItem>
+                <NavLink tag={Link} to='/'>
+                  Shows Dashboard
+                </NavLink>
+              </NavItem>
+              <li>
+                <span
+                    className='text-muted d-md-block d-none'
+                    style={{ padding: '8px', cursor: 'default' }}
+                >
+                  |
+                </span>
+              </li>
+              <NavItem>
+                <NavLink tag={Link} to='/portfolio'>
+                  Portfolio Dashboard
+                </NavLink>
+              </NavItem>
               <NavItem>
                 <Link to='/' className='nav-link' onClick={this.props.logout}>
                   Logout
